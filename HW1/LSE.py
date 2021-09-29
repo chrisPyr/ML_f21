47d70cee83509e4806310a2949c3d6096322e05d
chrisPyr <smile51293@gmail.com>
2021-09-29 17:19:58 +0800

    [HW1] done

--- HW1/LSE.py
+++ HW1/LSE.py
@@ -2,8 +2,34 @@
 
 import sys
 import numpy as np
+import argparse
+import matplotlib.pyplot as plt
 
+parser = argparse.ArgumentParser(description="HW1 program description")
+parser.add_argument("--file", type = str, default = "test.txt")
+parser.add_argument("--n", type = int,required = True, help="require input n(basis number)")
+parser.add_argument("--lamb", type = float, required = True, help = "require input lambda")
+args = parser.parse_args()
 #np.set_printoptions(precision = 20)
+
+def print_ans(ans,err):
+
+    print("Fitting", end=" ")
+    for x in range(len(ans)-1):
+        if x < len(ans):
+            sign = ""
+            if float(ans[x+1]) > 0:
+                sign = "+"
+            print("%.11f"%float(ans[x])+"X^"+str(len(ans)-x-1)+sign, end=" ")
+    sign = ""
+    if float(ans[len(ans)-1]) > 0:
+        sign = "+"
+    print(sign+"%.11f"%float(ans[len(ans)-1]))
+
+    print("Total Error: ", float(err))
+    return
+
+
 
 def matmul(A, B):
     row = len(A)
@@ -39,8 +65,7 @@ def identity(dim):
         idma[x,x] = 1
     return idma
 
-def LSE(AT,A,B):
-    matrix =  matmul(AT,A)
+def LU_decomposition(matrix):
     num_row,num_col = matrix.shape
     rows = np.size(matrix,0)
     L = identity(num_row)
@@ -53,19 +78,35 @@ def LSE(AT,A,B):
             re = (U[z,x] / base)
             L[z,x] = L[x,x] * re + L[z,x]
             U[z,:] = U[x,:] * -re + U[z,:]
+    return L,U
+
+def LSE(AT,A,B, lamb):
+    matrix =  matmul(AT,A)
+    for x in range(matrix.shape[0]):
+        matrix[x,x] += lamb
+    L, U = LU_decomposition(matrix)
 
     ans = LU_inverse(L, U, matmul(AT,B))
     err = error_sum(ans, A, B)
-    print(ans)
-    print("Fitting", end=" ")
-    for x in range(len(ans)-1):
-        if(ans[x])
-        print("%.11f"%float(ans[x])+"X^"+str(len(ans)-x-1)+" + ", end=" ")
+    print("LSE:")
+    print_ans(ans,err)
 
-    print("Total Error: ", err)
+    return ans
 
+def Newton_method(AT,A,B,lamb):
 
-    return
+    x = np.random.rand(A.shape[1],1)
+    #H(f(x))
+    Hessian = 2* matmul(AT,A)
+    #gradient
+    target = matmul(Hessian,x) - 2 * matmul(AT,B)
+    L,U = LU_decomposition(Hessian)
+    ans = x - LU_inverse(L,U,target)
+    err = error_sum(ans, A, B)
+    print("Newton's method:")
+    print_ans(ans,err)
+    return ans
+
 '''
 def LU_inverse(L, U):
     L_in = identity(L.shape[0])
@@ -116,28 +157,38 @@ def error_sum(para, A,B):
 
 
 
-def plot():
+def plot(A, B, A_extend, ans, ans_new):
+    plt.figure()
+    xmin = min(A)
+    xmax = max(A)
+    t = 0
+    x_label = np.arange(xmin-1,xmax+1,0.1)
+    plt.xlim(xmin-1,xmax+1)
+    plt.subplot(211)
+    plt.scatter(A,B)
+    for x in range(len(ans)):
+        t += ((x_label)**x)*ans[len(ans)-x-1]
+
+    plt.plot(x_label,t, color = 'red')
+    plt.subplot(212)
+    plt.scatter(A,B)
+    t = 0
+    for x in range(len(ans_new)):
+        t += ((x_label)**x)*ans_new[len(ans_new)-x-1]
+    plt.plot(x_label,t, color = 'red')
+    plt.show()
     return
 
 if __name__ == '__main__':
-    #A = np.matrix([[1,4,-3],[-2,8,5],[3,4,7]])
-
-    if len(sys.argv) < 2:
-        print("Please provide data file\n")
-        exit()
-
-    A, B = readdata(sys.argv[1])
+    A, B = readdata(args.file)
     A = np.asarray(A)
-    A_extend = np.zeros((A.shape[0],int(sys.argv[2])))
+    A_extend = np.zeros((A.shape[0],args.n))
     for z in range(A.shape[0]):
-        for k in range(int(sys.argv[2])):
-            A_extend[z,k] = A[z]**(int(sys.argv[2])-1-k)
-
-    #Amatrix = np.matrix(A)
+        for k in range(args.n):
+            A_extend[z,k] = A[z]**(args.n-1-k)
+    lamb = args.lamb
     AT = np.transpose(A_extend)
     B = np.asarray(B)
-    #print(arg)
-    #print(AT * A)
-    LSE(AT,A_extend,B)
-    #Bmatrix = np.matrix(B)
-    plot()
+    ans = LSE(AT,A_extend,B,lamb)
+    ans_new = Newton_method(AT,A_extend,B,lamb)
+    plot(A, B,A_extend, ans, ans_new)
